63019d023566e67995a041e4219c9673c8c425ff
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-07-17 20:01:08 +0530

    Added MAML scripts

--- scripts/train_maml_omniglot_uniform_sampling.sh
+++ scripts/train_maml_omniglot_uniform_sampling.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=maml_omniglot_uniform_sampling
-#SBATCH --output=../logs/maml_omniglot_uniform_sampling_%j.out
-#SBATCH --error=../logs/maml_omniglot_uniform_sampling_%j.err
+#SBATCH --output=../logs/maml_omniglot_uniform_sampling.out
+#SBATCH --error=../logs/maml_omniglot_uniform_sampling.err
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=20G
-#SBATCH --time=6:59:59
-#SBATCH --partition=unkillable
 
 source ../env/bin/activate
-cd .. && python -m src.main --train ./data --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 600 --output-folder ./config/
+cd .. && python -m src.main --train --runs 1 ./data --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 100 --output-folder ./config/maml_omniglot_uniform_sampling/
+python -m src.main ./data --output-folder ./config/maml_omniglot_uniform_sampling/
