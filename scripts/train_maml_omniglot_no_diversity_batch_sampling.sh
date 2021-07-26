56890e4f8363ceef97d9171a3462ac21917bda34
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-07-26 11:56:25 +0530

    Added MAML scripts

---
+++ scripts/train_maml_omniglot_no_diversity_batch_sampling.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=maml_omniglot_no_diversity_batch_sampling
+#SBATCH --output=../logs/maml_omniglot_no_diversity_batch_sampling.out
+#SBATCH --error=../logs/maml_omniglot_no_diversity_batch_sampling.err
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20G
+
+source ../env/bin/activate
+cd .. && python -m src.main --train --runs 1 ./data --task_sampler no_diversity_batch --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 600 --output-folder ./config/maml_omniglot_no_diversity_batch_sampling/
+python -m src.main ./data --output-folder ./config/maml_omniglot_no_diversity_batch_sampling/
--- scripts/train_maml_omniglot_uniform_sampling.sh
+++ scripts/train_maml_omniglot_uniform_sampling.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=maml_omniglot_uniform_sampling
 #SBATCH --output=../logs/maml_omniglot_uniform_sampling.out
 #SBATCH --error=../logs/maml_omniglot_uniform_sampling.err
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=20G
 
 source ../env/bin/activate
-cd .. && python -m src.main --train --runs 1 ./data --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 100 --output-folder ./config/maml_omniglot_uniform_sampling/
+cd .. && python -m src.main --train --runs 1 ./data --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 600 --output-folder ./config/maml_omniglot_uniform_sampling/
 python -m src.main ./data --output-folder ./config/maml_omniglot_uniform_sampling/
