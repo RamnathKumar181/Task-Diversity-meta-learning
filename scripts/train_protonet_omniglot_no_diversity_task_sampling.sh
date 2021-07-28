e9959ad0ef3b6c03ebf0f3b558419e4eeb8c1270
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-07-28 10:56:51 +0530

    Added protonet scripts

--- scripts/train_protonet_omniglot_no_diversity_task_sampling.sh
+++ scripts/train_protonet_omniglot_no_diversity_task_sampling.sh
@@ -7,5 +7,5 @@
 #SBATCH --mem=20G
 
 source ../env/bin/activate
-cd .. && python -m src.main --train --model protonet --runs 1 ./data --meta-lr 0.001 --task_sampler no_diversity_task --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 10000 --output-folder ./config/protonet_omniglot_no_diversity_task_sampling/
+cd .. && python -m src.main --train --model protonet --runs 1 ./data --meta-lr 0.001 --task_sampler no_diversity_task --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 100 --output-folder ./config/protonet_omniglot_no_diversity_task_sampling/
 python -m src.main ./data --output-folder ./config/protonet_omniglot_no_diversity_task_sampling/
