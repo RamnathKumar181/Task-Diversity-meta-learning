952584cd533b4281c18c23ec6449659e24269c60
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-07-28 20:25:31 +0530

    Added metaoptnet scripts

--- scripts/train_metaoptnet_omniglot_no_diversity_batch_sampling.sh
+++ scripts/train_metaoptnet_omniglot_no_diversity_batch_sampling.sh
@@ -7,5 +7,5 @@
 #SBATCH --mem=20G
 
 source ../env/bin/activate
-cd .. && python -m src.main --train --model metaoptnet --runs 1 ./data --meta-lr 0.001 --task_sampler no_diversity_batch --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 600 --output-folder ./config/metaoptnet_omniglot_no_diversity_batch_sampling/
+cd .. && python -m src.main --train --model metaoptnet --runs 1 ./data --meta-lr 0.1 --task_sampler no_diversity_batch --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 600 --output-folder ./config/metaoptnet_omniglot_no_diversity_batch_sampling/
 python -m src.main ./data --output-folder ./config/metaoptnet_omniglot_no_diversity_batch_sampling/
