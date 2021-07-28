36be258f45fad7de4b392988286578852259ec51
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-07-28 17:59:27 +0530

    Added protonet scripts

--- scripts/train_protonet_omniglot_uniform_sampling.sh
+++ scripts/train_protonet_omniglot_uniform_sampling.sh
@@ -7,5 +7,5 @@
 #SBATCH --mem=20G
 
 source ../env/bin/activate
-cd .. && python -m src.main --train --model protonet --runs 1 ./data --dataset omniglot --meta-lr 0.001 --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 100 --output-folder ./config/protonet_omniglot_uniform_sampling/
+cd .. && python -m src.main --train --model protonet --runs 1 ./data --dataset omniglot --meta-lr 0.001 --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 600 --output-folder ./config/protonet_omniglot_uniform_sampling/
 python -m src.main ./data --output-folder ./config/protonet_omniglot_uniform_sampling/
