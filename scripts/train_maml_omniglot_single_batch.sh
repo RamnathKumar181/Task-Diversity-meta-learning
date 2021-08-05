8c84017c2d864710b1ef77df50274ae285a9316b
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-08-05 22:33:16 +0530

    Updated scripts

---
+++ scripts/train_maml_omniglot_single_batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=maml_omniglot_single_batch
+#SBATCH --output=../logs/maml_omniglot_single_batch.out
+#SBATCH --error=../logs/maml_omniglot_single_batch.err
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20G
+
+source ../env/bin/activate
+cd .. && python -m src.main --train --runs 1 ./data --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 1 --num-workers 8 --num-epochs 600 --output-folder ./config/maml_omniglot_single_batch/
+python -m src.main ./data --output-folder ./config/maml_omniglot_single_batch/
