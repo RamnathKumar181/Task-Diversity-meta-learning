030bb5cee2caa3e3423d59acef5de63710fbe8be
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-08-14 17:14:02 +0530

    Added augmented dataset scripts

---
+++ scripts/Protonet/Train_omniglot/train_protonet_augmented_omniglot_no_diversity_tasks_per_batch_sampling.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=protonet_augmented_omniglot_no_diversity_tasks_per_batch_sampling
+#SBATCH --output=../logs/protonet_augmented_omniglot_no_diversity_tasks_per_batch_sampling.out
+#SBATCH --error=../logs/protonet_augmented_omniglot_no_diversity_tasks_per_batch_sampling.err
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20G
+
+source ../env/bin/activate
+cd .. && python -m src.main --exp_name protonet_augmented_omniglot_no_diversity_tasks_per_batch_sampling --use-random-crop --use-color-jitter --train --model protonet --runs 1 ./data --meta-lr 0.001 --task_sampler no_diversity_tasks_per_batch --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 50 --output-folder ./config/protonet_augmented_omniglot_no_diversity_tasks_per_batch_sampling/
