2d33f5587223b7121267a899848a0f47c74cf1c0
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-08-28 14:16:21 +0530

    Updated scripts structure

--- scripts/MAML/train_maml_augmented_omniglot_all_samplers.sh
+++ scripts/MAML/train_maml_augmented_omniglot_all_samplers.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=20G
-#SBATCH --array=0-5
+#SBATCH --array=6
 
 source ../env/bin/activate
 cd .. && python -m src.main --exp_name maml_augmented_omniglot --train --runs 1 --folder ./data --image-size 28 --task_sampler $SLURM_ARRAY_TASK_ID --dataset omniglot --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 150 --output-folder ./config/maml_augmented_omniglot/$SLURM_ARRAY_TASK_ID/
