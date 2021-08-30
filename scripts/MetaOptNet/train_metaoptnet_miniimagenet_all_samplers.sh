d7f38d944eb613b00711f7511b3300f0554c27f8
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-08-30 16:37:29 +0530

    Fixed scripts

---
+++ scripts/MetaOptNet/train_metaoptnet_miniimagenet_all_samplers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=metaoptnet_miniimagenet
+#SBATCH --output=../logs/metaoptnet_miniimagenet_%a.out
+#SBATCH --error=../logs/metaoptnet_miniimagenet_%a.err
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20G
+#SBATCH --array=0-7
+
+source ../env/bin/activate
+cd .. && python -m src.main --exp_name metaoptnet_miniimagenet --train --model metaoptnet --runs 1 --folder ./data --meta-lr 0.1 --task_sampler $SLURM_ARRAY_TASK_ID --dataset miniimagenet --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 60 --output-folder ./config/metaoptnet_miniimagenet/$SLURM_ARRAY_TASK_ID/
