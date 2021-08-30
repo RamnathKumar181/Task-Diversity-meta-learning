22ae375dd1471f8ad343d703025d1bc7287962d2
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-08-30 14:09:08 +0530

    Updated scripts

--- scripts/Matching_Networks/train_matching_networks_augmented_miniimagenet_all_samplers.sh
+++ scripts/Matching_Networks/train_matching_networks_augmented_miniimagenet_all_samplers.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=20G
-#SBATCH --array=6-7
+#SBATCH --array=0-7
 
 source ../env/bin/activate
 cd .. && python -m src.main --exp_name matching_networks_augmented_miniimagenet --train --model matching_networks --runs 1 --folder ./data --task_sampler $SLURM_ARRAY_TASK_ID --dataset miniimagenet --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 100 --output-folder ./config/matching_networks_augmented_miniimagenet/$SLURM_ARRAY_TASK_ID/
