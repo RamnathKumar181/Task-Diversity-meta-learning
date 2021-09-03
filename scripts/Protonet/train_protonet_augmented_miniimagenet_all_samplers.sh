b56594500da2dd96f3bb31d0600b307117821add
RamnathKumar181 <ramnathkumar181@gmail.com>
2021-09-03 23:10:47 +0530

    Updated scripts structure

--- scripts/Protonet/train_protonet_augmented_miniimagenet_all_samplers.sh
+++ scripts/Protonet/train_protonet_augmented_miniimagenet_all_samplers.sh
@@ -9,4 +9,4 @@
 
 source ../env/bin/activate
 cd ..
-python -m src.main --exp_name protonet_augmented_miniimagenet --train --model protonet --runs 1 --folder ./data --meta-lr 0.001 --task_sampler $SLURM_ARRAY_TASK_ID --dataset miniimagenet --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 100 --output-folder ./config/protonet_augmented_miniimagenet/$SLURM_ARRAY_TASK_ID/
+python -m src.main --exp_name protonet_augmented_miniimagenet --train --model protonet --use-augmentations --runs 1 --folder ./data --meta-lr 0.001 --task_sampler $SLURM_ARRAY_TASK_ID --dataset miniimagenet --num-ways 5 --num-shots 1 --use-cuda --step-size 0.4 --batch-size 32 --num-workers 8 --num-epochs 100 --output-folder ./config/protonet_augmented_miniimagenet/$SLURM_ARRAY_TASK_ID/
